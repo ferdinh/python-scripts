e3576d30e6997a39b23d4b1e864203922cfac26d
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2018-09-02 23:19:47 +1200

    Add test for parsing to RPN and evaluating RPN

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -56,3 +56,40 @@ class TestCalculatorClass(object):
 
         # assert
         assert expectedResult == actualResult
+
+    def test_parse_to_rpn(self):
+        # Arrange  
+        # Case 1
+        expected_case_1 = [6.0, 8.0, "/", 9.0, "+", 8.0, 10.0, "*", "-"]
+        infix_case_1 = "6 / 8 + 9 - 8 * 10"
+
+        # Case 2
+        expected_case_2 = [3.0, 2.0, "*", 5.0, "+"]
+        infix_case_2 = "3 * 2 + 5"
+
+        # act
+        result_case_1 = mathematics.parse_to_rpn(infix_case_1)
+        result_case_2 = mathematics.parse_to_rpn(infix_case_2)
+
+        # assert
+        assert result_case_1 == expected_case_1
+        assert result_case_2 == expected_case_2
+
+    def test_evaluate_rpn(self):
+
+        #Arrange
+        # Case 1
+        expected_case_1 = -70.25
+        infix_case_1 = [6.0, 8.0, "/", 9.0, "+", 8.0, 10.0, "*", "-"]
+
+        # Case 2
+        expected_case_2 = 11.0
+        infix_case_2 = [3.0, 2.0, "*", 5.0, "+"]
+
+        # act
+        result_case_1 = mathematics.evaluate_rpn(infix_case_1)
+        result_case_2 = mathematics.evaluate_rpn(infix_case_2)
+
+        # assert
+        assert result_case_1 == expected_case_1
+        assert result_case_2 == expected_case_2
