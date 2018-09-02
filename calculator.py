fe4badfbc43ddad4282a907e174d4bfa55921d1b
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2018-09-02 23:54:25 +1200

    Modify to utilize parser

--- calculator.py
+++ calculator.py
@@ -5,38 +5,10 @@ import mathematics
 
 def main():
 
-    check = True
-
-    while True:
-        try:
-            x = float(input("Enter 1st number: "))
-            y = float(input("Enter 2nd number: "))
-        except ValueError:
-            print("Please enter a valid input!")
-        else:
-            break
-
-    result = 0.0
-
-    while check:
-        choice = input("Choose an arithmetic operation (add, min, multiply, divide): ")
-
-        if choice == "add":
-            result = mathematics.add(x, y)
-            check = False
-        elif choice == "min":
-            result = mathematics.min(x, y)
-            check = False
-        elif choice == "multiply":
-            result = mathematics.multiply(x, y)
-            check = False
-        elif choice == "divide":
-            result = mathematics.divide(x, y)
-            check = False
-        else:
-            print("Please enter a valid arithmetic operation!")
-
-    print(f"The result is {result:.3f}")
+    userInput = input()
+    result = mathematics.evaluate(userInput)
+    print(result)
+    
     return 0
 
 
