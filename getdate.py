cb552ee9febbf131926788c4e725731824e22ac5
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-04-01 16:09:10 +1300

    Reimplement main function
    Program no longer ask for hours, minutes and seconds.
    
    Secondly, current time is updated whenever the user selects an option instead of only updating when the program starts.

--- getdate.py
+++ getdate.py
@@ -9,31 +9,6 @@ class TimeCoordinate(Enum):
     Local = 2
 
 
-today = datetime.datetime.today()
-utc = datetime.datetime.utcnow()
-
-print("Current time: ", today)
-print("UTC time: ", utc)
-
-x = 0
-
-while x == 0:
-    z = input("View UTC or local? ")
-    y = input("View hours, minutes, or seconds, or terminate? ")
-    if z == "local" and y == "hours":
-        print(today.hour)
-    elif z == "local" and y == "minutes":
-        print(today.minute)
-    elif z == "local" and y == "seconds":
-        print(today.second)
-    elif z == "UTC" and y == "hours":
-        print(utc.hour)
-    elif z == "UTC" and y == "seconds":
-        print(utc.second)
-    elif z == "UTC" and y == "minutes":
-        print(utc.minute)
-    else:
-        x = 1
 def getTime(localization: TimeCoordinate = TimeCoordinate.Local) -> datetime:
     """Get current datetime based on specified localization"""
     currentTime: datetime
@@ -44,3 +19,25 @@ def getTime(localization: TimeCoordinate = TimeCoordinate.Local) -> datetime:
         currentTime = datetime.datetime.today()
 
     return currentTime
+
+
+def main():
+    isRunning: bool = True
+
+    while isRunning:
+        localStr: str = input("View UTC or local time? ")
+
+        # Use local time if user input is invalid.
+        localEnum = TimeCoordinate.Local
+
+        # Convert str to TimeCoordinate
+        if localStr.lower() == "local":
+            localEnum = TimeCoordinate.Local
+        elif localStr.lower() == "utc":
+            localEnum = TimeCoordinate.UTC
+
+        print("Current time is: " + str(getTime(localEnum)))
+
+
+if __name__ == "__main__":
+    main()
